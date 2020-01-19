d5ee66234aab3f052c6ada8a507177acb460bf2c
indegser <indegser@gmail.com>
2020-01-19 22:05:31 +0900

    add migrate

---
+++ migrations/sqls/20200119125124-story-down.sql
@@ -0,0 +1,2 @@
+/* Replace with your SQL commands */
+delete table story;
---
+++ migrations/sqls/20200119125124-story-up.sql
@@ -0,0 +1,7 @@
+/* Replace with your SQL commands */
+create table story (
+  id text primary key,
+  title text not null,
+  excerpt text not null,
+  body text
+);
