acca0483e92c9276514a5d88cbef075777f7bdd4
indegser <indegser@gmail.com>
2020-01-21 13:39:30 +0900

    add error handling

--- pages/api/story/[storyId].ts
+++ pages/api/story/[storyId].ts
@@ -1,14 +1,24 @@
 import cors from 'micro-cors'
 import nanoid from 'nanoid'
+import { NotFoundError } from 'slonik'
 import { db, sql } from '../../../src/database'
 
 const middy = () => responder => async (req, res) => {
+  const requestId = nanoid(7)
   try {
     const data = await cors()(responder)(req, res)
-    res.json({ requestId: nanoid(7), data })
+    res.json({ requestId, data, error: null })
   } catch (err) {
-    console.log(err)
-    res.json({ err })
+    if (err instanceof NotFoundError) {
+      res.statusCode = 404
+    } else {
+      res.statusCode = 400
+    }
+    res.json({
+      requestId,
+      data: null,
+      error: err.message,
+    })
   }
 }
 
@@ -17,9 +27,11 @@ const handler = async (req, res) => {
 
   switch (req.method) {
     case 'DELETE': {
-      await db.query(sql`
+      const result = await db.one(sql`
         delete from story where id = ${storyId}
+        returning story.id
       `)
+
       return { storyId }
     }
     case 'GET': {
