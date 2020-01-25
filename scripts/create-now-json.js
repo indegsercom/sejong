7f2fb6fe0fb4032ad9151f286ab95fd1630a24df
indegser <indegser@gmail.com>
2020-01-25 17:40:55 +0900

    update ci yml

--- scripts/create-now-json.js
+++ scripts/create-now-json.js
@@ -1,10 +1,10 @@
 const fs = require('fs')
 
 console.log('>>> Creating now.json with github env variables.')
-const { DATABASE_URL, NOW_ALIAS } = process.env
+const { DATABASE_URL, NOW_NAME } = process.env
 const json = JSON.parse(fs.readFileSync('now.json', 'utf-8'))
 json.env.DATABASE_URL = DATABASE_URL
-json.alias = NOW_ALIAS
+json.name = NOW_NAME
 
 fs.writeFileSync('now.json', JSON.stringify(json), 'utf-8')
 
