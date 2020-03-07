468409dafb6d02a8e92767b8b82f2e1c68b84f84
Jaekwon Han <han.jaekwon@naverlabs.com>
2020-03-07 15:23:57 +0900

    Use graphql in book api

--- pages/api/book.ts
+++ pages/api/book.ts
@@ -1,17 +1,53 @@
-import bookService from '../../src/services/bookService'
-import handler from '../../src/handler'
+import bookService from 'services/bookService'
+import { createApolloServer } from 'handler'
+import { gql } from 'apollo-server-micro'
+import { nodeTypeDefs } from 'graphql/typeDefs'
+import { BookTable } from 'db/tableTypes'
 
-const responder = async (req, res) => {
-  switch (req.method) {
-    case 'GET': {
-      const books = await bookService.getAll()
-      res.json({ data: { books } })
-    }
-    case 'POST': {
-      const book = await bookService.create(req.body)
-      res.json({ data: { book } })
-    }
+const typeDefs = gql`
+  type Query {
+    getBooks: [Book]
   }
+
+  type Book {
+    ${nodeTypeDefs}
+    title: String
+    cover: String
+    citation: String
+  }
+
+  input CreateBookInput {
+    title: String
+    cover: String
+    authors: [String]
+    publishedYear: Int
+  }
+
+  type Mutation {
+    createBook(book: CreateBook): Book
+  }
+`
+
+const resolvers = {
+  Query: {
+    getBooks: () => bookService.getAll(),
+  },
+  Mutation: {
+    createBook: (_, { book }: { book: BookTable }) => {
+      return bookService.create({
+        table: 'book',
+        ...book,
+      })
+    },
+  },
+  Book: {
+    citation: (book: BookTable) => {
+      return `${book.authors.join(', ')} (${book.publishedYear})}`
+    },
+  },
 }
 
-export default handler()(responder)
+export default createApolloServer('api/book', {
+  typeDefs,
+  resolvers,
+})
--- src/handler.ts
+++ src/handler.ts
@@ -1,6 +1,3 @@
-import { HttpError } from 'http-errors'
-import { NotFoundError, UniqueIntegrityConstraintViolationError } from 'slonik'
-import cors from './cors'
 import Cors from 'micro-cors'
 import { Config, ApolloServer } from 'apollo-server-micro'
 import { NextApiResponse } from 'next'
@@ -11,7 +8,7 @@ export const apolloServerConfig = {
   },
 }
 
-const cors2 = Cors({
+const cors = Cors({
   allowMethods: ['POST', 'GET', 'OPTIONS'],
 })
 
@@ -30,43 +27,5 @@ export const createApolloServer = (path: string, config: Config) => {
     apolloHandler(req, res)
   }
 
-  return cors2(handler)
+  return cors(handler)
 }
-
-const handler = () => (...mids) => async (req, res) => {
-  const middlewares = [cors(), ...mids]
-  let middlewareIdx = 0
-
-  const next = () => {
-    middlewareIdx++
-  }
-
-  try {
-    while (middlewareIdx < middlewares.length) {
-      let currentIdx = middlewareIdx
-      const middleware = middlewares[middlewareIdx]
-      await middleware(req, res, next)
-
-      if (currentIdx === middlewareIdx) {
-        // next is not called
-        break
-      }
-    }
-  } catch (err) {
-    if (err instanceof NotFoundError) {
-      res.statusCode = 404
-    } else if (err instanceof HttpError) {
-      res.statusCode = err.statusCode
-    } else if (err instanceof UniqueIntegrityConstraintViolationError) {
-      res.statusCode = 409
-    } else {
-      res.statusCode = 400
-    }
-    res.json({
-      data: null,
-      error: err.message,
-    })
-  }
-}
-
-export default handler
