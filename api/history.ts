2ee2d52ed7a33cd6e14e7ce3a2ccfc453c235590
Jaekwon Han <han.jaekwon@naverlabs.com>
2020-04-03 02:46:02 +0900

    Connect graphql to micro

---
+++ api/history.ts
@@ -0,0 +1,52 @@
+import historyService from '../../_src/services/historyService'
+import { gql } from 'apollo-server-micro'
+import { nodeTypeDefs } from 'graphql/typeDefs'
+import { createApolloServer, apolloServerConfig } from 'handler'
+import isAuthenticated from 'graphql/resolvers/isAuthenticated'
+import combine from 'graphql/resolvers/combine'
+
+const typeDefs = gql`
+  type Query {
+    getHistories: [History]
+  }
+
+  input CreateHistoryInput {
+    link: String!
+    comment: String
+  }
+
+  type Mutation {
+    createHistory(input: CreateHistoryInput): History
+    deleteHistory(id: ID!): Boolean
+  }
+
+  type History {
+    ${nodeTypeDefs}
+    title: String!
+    excerpt: String!
+    link: String!
+    cover: String
+    comment: String
+  }
+`
+
+const resolvers = {
+  Query: {
+    getHistories: () => historyService.getHistories(),
+  },
+  Mutation: {
+    createHistory: combine(isAuthenticated, (_, { input }) =>
+      historyService.createHistory(input)
+    ),
+    deleteHistory: combine(isAuthenticated, (_, { id }) =>
+      historyService.deleteHistory(id)
+    ),
+  },
+}
+
+export const config = apolloServerConfig
+
+export default createApolloServer('/api/history', {
+  typeDefs,
+  resolvers,
+})
--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,29 @@
-import { send } from 'micro'
-import { ServerResponse, IncomingMessage } from 'http'
+import micro from 'micro'
+import { ApolloServer, gql } from 'apollo-server-micro'
 
-export default (req: IncomingMessage, res: ServerResponse) => {
-  send(res, 200)
+const typeDefs = gql`
+  type Query {
+    sayHello: String
+  }
+`
+
+const resolvers = {
+  Query: {
+    sayHello() {
+      return 'Hello world'
+    },
+  },
 }
+
+const server = new ApolloServer({
+  typeDefs,
+  resolvers,
+  playground: true,
+  // introspection: true,
+})
+
+const PORT = process.env.PORT || 3000
+
+micro(server.createHandler()).listen(PORT, () =>
+  console.log(`Micro on port: ${PORT}`)
+)
