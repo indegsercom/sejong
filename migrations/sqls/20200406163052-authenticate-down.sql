a4edd363ec0aa4acbbdfe796e2f1b41ad7debe9e
Jaekwon Han <han.jaekwon@naverlabs.com>
2020-04-07 02:22:32 +0900

    Add migrations for postgraphile

---
+++ migrations/sqls/20200406163052-authenticate-down.sql
@@ -0,0 +1,7 @@
+/* Replace with your SQL commands */
+drop function authenticate(text, text);
+drop function _private.register(text, text);
+
+drop table _private.account;
+drop type jwt_token;
+drop schema _private;
---
+++ migrations/sqls/20200406171715-add-modified-at-index-down.sql
@@ -0,0 +1,2 @@
+drop INDEX history_published_at_index;
+drop INDEX book_published_at_index;
